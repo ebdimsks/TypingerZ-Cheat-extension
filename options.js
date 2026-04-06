18e79c6d65a132e555a58e461c813cb8fbdc6993
ebdimsks <iloveoss@proton.me>
2026-04-06 15:36:56 +0900

    commit

--- options.js
+++ options.js
@@ -1,6 +1,66 @@
-const DEFAULT_CODE = `// ここに実行したい JS を保存してください。
-// 例:
-console.log("[Typingerz JS Runner] loaded:", location.href);`;
+const DEFAULT_CODE = `function tyTex(text) {
+  let typeTarget = text.split("");
+
+  typeTarget.forEach((character) => {
+    if (character === "n") {
+      let customKeyEvent = new KeyboardEvent('keydown', {
+        key: character,
+        code: "KeyN",
+        location: 0,
+        ctrlKey: false,
+        shiftKey: false,
+        altKey: false,
+        metaKey: false,
+        repeat: false,
+        isComposing: false,
+        charCode: 0,
+        keyCode: 78,
+        which: 78,
+      });
+
+      document.dispatchEvent(customKeyEvent);
+
+      customKeyEvent = new KeyboardEvent('keyup', {
+        key: character,
+        code: "KeyN",
+        location: 0,
+        ctrlKey: false,
+        shiftKey: false,
+        altKey: false,
+        metaKey: false,
+        repeat: false,
+        isComposing: false,
+        charCode: 0,
+        keyCode: 78,
+        which: 78,
+      });
+
+      document.dispatchEvent(customKeyEvent);
+    } else {
+      let keyEvent = new KeyboardEvent('keydown', {
+        key: character,
+        code: `Key${character.toUpperCase()}`
+      });
+
+      document.dispatchEvent(keyEvent);
+
+      let keyUpEvent = new KeyboardEvent('keyup', {
+        key: character,
+        code: `Key${character.toUpperCase()}`
+      });
+
+      document.dispatchEvent(keyUpEvent);
+    }
+  });
+}
+
+
+function handMond() {
+  let textContent = document.getElementById("mondaifield").textContent;
+  tyTex(textContent);
+}
+
+setInterval(handMond, 10);`;
 
 const codeEl = document.getElementById("code");
 const saveBtn = document.getElementById("save");
